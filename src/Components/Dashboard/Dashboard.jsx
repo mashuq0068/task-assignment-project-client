c0cfa0f4e6b0b31695b37e3e0740b38a6c8d94b8
mashuq0068 <mashuq0068@gmail.com>
2023-12-22 00:26:33 +0600

    added dragge

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import useTasks from "../../Hooks/UseTasks";
+import Draggable from "react-draggable";
+import { Droppable } from "react-beautiful-dnd";
 
 
 const Dashboard = () => {
@@ -17,13 +19,17 @@ const Dashboard = () => {
             tasks?.map(task => 
                
                
-            <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
+          
+             <Draggable key={task?._id}>
+                <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
                 <p>{task?.taskTitle?.slice(0,6)}</p>
                 <button className="btn btn-ghost">See Details</button>
                 <button className="btn btn-ghost">Delete</button>
                
             </div>
           
+            </Draggable>
+        
             )
             }
             </div>
@@ -35,14 +41,14 @@ const Dashboard = () => {
            {
             tasks?.map(task => 
                
-               
+             <Draggable key={task?._id}>
             <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
                 <p>{task?.taskTitle?.slice(0,6)}</p>
                 <button className="btn btn-ghost">See Details</button>
                 <button className="btn btn-ghost">Delete</button>
                
             </div>
-          
+            </Draggable>  
             )
             }
             </div>
@@ -54,13 +60,14 @@ const Dashboard = () => {
            {
             tasks?.map(task => 
                
-               
+               <Draggable key={task?._id}>
             <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
                 <p>{task?.taskTitle?.slice(0,6)}</p>
                 <button className="btn btn-ghost">See Details</button>
                 <button className="btn btn-ghost">Delete</button>
                
             </div>
+            </Draggable>
           
             )
             }
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
 import AuthProvider from './Providers/AuthProvider.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+   
      <QueryClientProvider client={queryClient}>
    <AuthProvider>
    <RouterProvider router={router}></RouterProvider>
    </AuthProvider>
    </QueryClientProvider>
+  
   </React.StrictMode>,
 )
