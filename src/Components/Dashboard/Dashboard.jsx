a4f86c73080494c458c8e3e8319b33b52912eef5
mashuq0068 <mashuq0068@gmail.com>
2023-12-22 01:17:58 +0600

    updated drag

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import useTasks from "../../Hooks/UseTasks";
 import Draggable from "react-draggable";
-import { Droppable } from "react-beautiful-dnd";
+
 
 
 const Dashboard = () => {
@@ -10,9 +10,9 @@ const Dashboard = () => {
     return (
         <div>
          <h1 className=" text-2xl mb-[5vh] font-bold text-center mt-[5vh]"> Task management dashboard</h1>
-         <Link to='/createTask' className=" btn flex justify-center items-center fixed right-5 bg-white 2xl:text-lg rounded-xl font-bold drop-shadow-xl shadow-xl"><p className=" text-2xl 2xl:text-3xl">+</p> <p>Create Task</p></Link>
-         <div className="flex  flex-wrap">
-         <div className= " drop-shadow-xl space-y-5 lg:w-[30vw] p-[2%] shadow-xl">
+         <Link to='/createTask' className=" btn flex justify-center items-center fixed right-5 bg-white 2xl:text-lg rounded-xl font-bold "><p className=" text-2xl 2xl:text-3xl">+</p> <p>Create Task</p></Link>
+         <div className="flex absolute flex-wrap">
+         <div className= "  space-y-5 lg:w-[30vw] p-[2%] ">
          <h3  className=" text-xl font-bold text-center"> to-do list Tasks</h3>
          <div>
            {
@@ -35,45 +35,8 @@ const Dashboard = () => {
             </div>
       
          </div>
-         <div className= " drop-shadow-xl space-y-5 lg:w-[30vw] p-[2%] shadow-xl">
-         <h3  className=" text-xl font-bold text-center"> Ongoing Tasks</h3>
-         <div>
-           {
-            tasks?.map(task => 
-               
-             <Draggable key={task?._id}>
-            <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
-                <p>{task?.taskTitle?.slice(0,6)}</p>
-                <button className="btn btn-ghost">See Details</button>
-                <button className="btn btn-ghost">Delete</button>
-               
-            </div>
-            </Draggable>  
-            )
-            }
-            </div>
-         </div>
-         
-         <div className= " drop-shadow-xl space-y-5 lg:w-[30vw] p-[2%] shadow-xl">
-         <h3  className=" text-xl font-bold text-center"> Completed list Tasks</h3>
-         <div>
-           {
-            tasks?.map(task => 
-               
-               <Draggable key={task?._id}>
-            <div className=" grid px-[2%] py-[1%] rounded-lg border border-gray-300 grid-cols-3 " key={task._id}>
-                <p>{task?.taskTitle?.slice(0,6)}</p>
-                <button className="btn btn-ghost">See Details</button>
-                <button className="btn btn-ghost">Delete</button>
-               
-            </div>
-            </Draggable>
-          
-            )
-            }
-            </div>
-         </div>
-         
+         <h3  className=" text-xl absolute top-0 left-[40vw] w-[10vw] font-bold text-center"> Ongoing Tasks</h3>
+         <h3  className=" text-xl absolute top-0 left-[70vw] w-[10vw] font-bold text-center"> completed Tasks</h3>
         </div>
         </div>
     );
